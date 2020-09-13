296253f166aa7c02f762f46d19133c73cda57042
ankita-de-222000 <ankita.de10@gmail.com>
2020-09-13 17:57:02 +0530

    Initial Commit

--- app/src/main/java/com/example/pujo360/MainActivity.java
+++ app/src/main/java/com/example/pujo360/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.pujo360;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import co.gofynd.gravityview.GravityView;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView img;
-    private GravityView gravityView;
-    private boolean esSoportado= false;
+
+    private Button ImageView360;
 
 
     @Override
@@ -20,30 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
-        if(esSoportado){
-            this.gravityView.setImage(img, R.drawable.panorama).center();
-        }else{
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.panorama);
-            img.setImageBitmap(bitmap);
-        }
-    }
-    private void init(){
-        this.img = findViewById(R.id.imageView);
-        this.gravityView = GravityView.getInstance(getBaseContext());
-        this.esSoportado =gravityView.deviceSupported();
+        ImageView360 = findViewById(R.id.Viewer);
+        ImageView360.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ImageViewer360.class);
+                startActivity(i);
+            }
+        });
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        gravityView.registerListener();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        gravityView.unRegisterListener();
-    }
 }
