0fc7bdc5b88c912a87d175bdd377215e628b174c
Madhurima <madhumukherjee1999@gmail.com>
2020-09-14 21:40:16 +0530

    Registration

---
+++ app/src/main/java/com/example/pujo360/RegistrationPujoCommittee.java
@@ -0,0 +1,14 @@
+package com.example.pujo360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationPujoCommittee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_pujo_committee);
+    }
+}
---
+++ app/src/main/java/com/example/pujo360/registration_choice.java
@@ -0,0 +1,14 @@
+package com.example.pujo360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_choice);
+    }
+}
