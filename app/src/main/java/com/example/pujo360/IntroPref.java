98eb14f45fd26bc7e2d85f050bd19a820d4d2ad5
Sarbari <applex.inc03@gmail.com>
2020-09-14 16:52:42 +0530

    commit

---
+++ app/src/main/java/com/example/pujo360/IntroPref.java
@@ -0,0 +1,185 @@
+package com.example.pujo360;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+public class IntroPref {
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private Context context;
+    private int PRIVATE_MODE = 0;
+    private static final String PREF_NAME = "com.applex.campus24.users";
+    private static final String IS_FIRST_TIME_LAUNCH = "firstTime";
+    private static final String USERDP = "userdp";
+    private static final String FULLNAME = "fullname";
+    private static final String DEFAULTDP = "defaultdp";
+    private static final String TYPE= "type";
+    private static final String GENDER= "gender";
+    private static final String ACCOUNT= "account";
+    private static final String FOLDER = "preptotal";
+    private static final String BATCHID = "batchID";
+    private static final String CITY = "city";
+    private Gson gson;
+
+    @SuppressLint("CommitPrefEdits")
+    public IntroPref(Context context){
+        this.context = context;
+        if(context != null) {
+            preferences = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        }
+        editor = preferences.edit();
+        gson = new Gson();
+    }
+
+    void setIsFirstTimeLaunch(boolean firstTimeLaunch)
+    {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH,firstTimeLaunch);
+        editor.commit();
+    }
+
+    boolean isFirstTimeLaunch(){
+        return preferences.getBoolean(IS_FIRST_TIME_LAUNCH,true);
+    }
+
+
+    //GENDER////
+    public String getGender(){
+        return preferences.getString(GENDER, null);
+    }
+
+    public void setGender(String gender){
+        editor.putString(GENDER, gender);
+        editor.apply();
+    }
+    //GENDER////
+
+    //CITY////
+    public String getCity(){
+        return preferences.getString(CITY, null);
+    }
+
+    public void setCity(String city){
+        editor.putString(CITY, city);
+        editor.apply();
+    }
+    //CITY////
+
+    ///USERDP///
+    public String getUserdp(){
+        return preferences.getString(USERDP, null);
+    }
+
+    public void setUserdp(String userdp){
+        editor.putString(USERDP, userdp);
+        editor.apply();
+    }
+    ///USERDP///
+
+    ///DEFAULTDP///
+    public String getDefaultdp(){
+        return preferences.getString(DEFAULTDP, null);
+    }
+
+    public void setDefaultdp(String defaultdp){
+        editor.putString(DEFAULTDP, defaultdp);
+        editor.apply();
+    }
+    ///DEFAULTDP///
+
+
+    ///FULLNAME///
+    public String getFullName(){
+        return preferences.getString(FULLNAME, null);
+    }
+
+    public void setFullName(String fullName){
+        editor.putString(FULLNAME, fullName);
+        editor.apply();
+    }
+    ///FULLNAME///
+
+    ///TYPE///
+    public String getType(){
+        return preferences.getString(TYPE, null);
+    }
+
+    public void setType(String type){
+        editor.putString(TYPE, type);
+        editor.apply();
+    }
+    ///TYPE///
+
+    ///PREPTOTAL FOLDER///
+    public String getFolderID() { return preferences.getString(FOLDER, null); }
+
+    public void setFolderID(String folderID) {
+        editor.putString(FOLDER, folderID);
+        editor.apply();
+    }
+    ///PREPTOTAL FOLDER///
+
+    ///ALL OTHER FOLDERS///
+    public String getSpecificFolderID(String folder) { return preferences.getString(folder, null); }
+
+    public void setSpecificFolderID(String folder, String folderID) {
+        editor.putString(folder, folderID);
+        editor.apply();
+    }
+    ///ALL OTHER FOLDERS///
+
+    ///BATCH ID///
+    public String getBatchID() { return preferences.getString(BATCHID, null); }
+
+    public void setBatchID(String batchID) {
+        editor.putString(BATCHID, batchID);
+        editor.apply();
+    }
+    ///BATCH ID///
+
+    public GoogleSignInAccount getGoogleSignInAccount() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Uri.class, new UriDeserializer())
+                .create();
+        String json = preferences.getString(ACCOUNT, "");
+        return gson.fromJson(json, GoogleSignInAccount.class);
+    }
+
+    public void setGoogleSignInAccount(GoogleSignInAccount googleSignInAccount) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Uri.class, new UriSerializer())
+                .create();
+        String json = gson.toJson(googleSignInAccount);
+        editor.putString(ACCOUNT, json);
+        editor.apply();
+    }
+
+    public static class UriSerializer implements JsonSerializer<Uri> {
+        public JsonElement serialize(Uri src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.toString());
+        }
+    }
+
+    public static class UriDeserializer implements JsonDeserializer<Uri> {
+        @Override
+        public Uri deserialize(final JsonElement src, final Type srcType,
+                               final JsonDeserializationContext context) throws JsonParseException {
+            return Uri.parse(src.getAsString());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pujo360/util/Utility.java
@@ -0,0 +1,135 @@
+package com.example.pujo360.util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.MediaStore;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
+public class Utility {
+
+//    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final String TAG = "SAVE_IMAGE_BITMAP_NOT_WORKING";
+    public static Long tsLong;
+//    private String[] cameraPermission;
+    /**
+     * CHECK WHETHER INTERNET CONNECTION IS AVAILABLE OR NOT
+     */
+
+    public static void showToast(Context context, String data) {
+        Toast.makeText(context, data, Toast.LENGTH_SHORT).show();
+    }
+
+    public static Boolean saveImage(Bitmap finalBitmap, Context context) {
+        File myDir = new File(context.getExternalFilesDir(null), "/Campus24");
+        if (!myDir.exists()) {
+            myDir.mkdirs();
+        }
+        tsLong = System.currentTimeMillis();
+        try {
+            File file = new File(myDir, "IMG-" + tsLong + ".jpg");
+            file.createNewFile();
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            MediaStore.Images.Media.insertImage(context.getContentResolver(), finalBitmap, "IMG-" + tsLong + ".jpg" , "Downloaded from Campus24");
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public static String getTimeAgo(Long timestamp) {
+        int SECOND_MILLIS = 1000;
+        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        int DAY_MILLIS = 24 * HOUR_MILLIS;
+        double time = 0.0;
+        try {
+            Double a= Double.valueOf(timestamp);
+            time = Math.round(a);
+        }catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+        // TODO: localize
+        final long diff = (long) (now - time);
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a min ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " mins ago";
+        } else if (diff < 120 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff >= 2 * HOUR_MILLIS && diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
+
+
+
+    public static String getFittingString(String s, int length) {
+        if (s.length() > length) {
+            s = s.substring(0, length) + "\u2026";
+        }
+        return s;
+    }
+
+    public static void hideKeyboard(Context context, ConstraintLayout layout) {
+        InputMethodManager imm = (InputMethodManager)context.getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(layout.getWindowToken(), 0);
+    }
+
+    public static void vibrate(Context context) {
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(50);
+        }
+    }
+
+    public static void requestStoragePermission(Context mContext) {
+        String[] storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions((Activity) mContext, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    public static boolean checkStoragePermission(Context mContext) {
+        return ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+    }
+
+}
