3b834ac79b3e677eb2b607cc1ca8ce718c51fcda
Madhurima <madhumukherjee1999@gmail.com>
2020-09-15 00:30:50 +0530

    Registration

--- app/src/main/java/com/example/pujo360/models/BaseUserModel.java
+++ app/src/main/java/com/example/pujo360/models/BaseUserModel.java
@@ -1,4 +1,61 @@
 package com.example.pujo360.models;
 
 public class BaseUserModel {
+    private String name;
+    private String addressline;
+    private String city;
+    private String state;
+    private String dp;
+    private String coverpic;
+
+    private String uid;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressline() {
+        return addressline;
+    }
+
+    public void setAddressline(String addressline) {
+        this.addressline = addressline;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDp() {
+        return dp;
+    }
+
+    public void setDp(String dp) {
+        this.dp = dp;
+    }
+
+    public String getCoverpic() {
+        return coverpic;
+    }
+
+    public void setCoverpic(String coverpic) {
+        this.coverpic = coverpic;
+    }
 }
