83c9a91f1cd59c347856dcc3c8f6aa9f5d2296fa
Sarbari <applex.inc03@gmail.com>
2020-09-15 03:34:28 +0530

    commit

--- app/src/main/java/com/example/pujo360/models/IndividualModel.java
+++ app/src/main/java/com/example/pujo360/models/IndividualModel.java
@@ -2,16 +2,26 @@ package com.example.pujo360.models;
 
 public class IndividualModel {
 
-    private String fullname;
+    private String firstname,lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     private String gender;
 
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
 
     public String getGender() {
         return gender;
