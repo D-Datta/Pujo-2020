db0dfe5821be677544a55644c781e9f9b6f58a06
ankita-de-222000 <ankita.de10@gmail.com>
2020-09-17 02:11:00 +0530

    Profile Activity Created

---
+++ app/src/main/java/com/example/pujo360/ActivityProfileUser.java
@@ -0,0 +1,16 @@
+package com.example.pujo360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityProfileUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_user);
+
+
+    }
+}
