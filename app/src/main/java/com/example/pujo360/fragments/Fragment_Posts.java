3a2d378d4d74947a2a9cc3f017188810885bb135
Sourajit <amisourajit123@gmail.com>
2020-09-24 04:22:18 +0530

    init

--- app/src/main/java/com/example/pujo360/fragments/Fragment_Posts.java
+++ app/src/main/java/com/example/pujo360/fragments/Fragment_Posts.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -36,7 +37,7 @@ public class Fragment_Posts extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerview;
     private ProgressBar contentprogressposts,progressmoreposts;
-    private ImageView noneImage;
+    private NestedScrollView noneImage;
     private FirestorePagingAdapter adapter;
 
 
