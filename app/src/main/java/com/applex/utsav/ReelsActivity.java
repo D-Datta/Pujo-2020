be1fab0c35c2ee5ee45b27b0ebd234c92994f176
prajata22 <prajatasamanta22@gmail.com>
2020-09-28 21:02:49 +0530

    Pujo App

--- app/src/main/java/com/applex/utsav/ReelsActivity.java
+++ app/src/main/java/com/applex/utsav/ReelsActivity.java
@@ -50,72 +50,35 @@ public class ReelsActivity extends AppCompatActivity {
                 ReelsPostModel reelsPostModel = document.toObject(ReelsPostModel.class);
                 Objects.requireNonNull(reelsPostModel).setDocID(document.getId());
                 models.add(reelsPostModel);
-                fetchAfter(bool, document);
-                fetchBefore(bool, document);
+                Query query = null;
+
+                if(bool.matches("1")) {
+                    query = FirebaseFirestore.getInstance()
+                            .collection("Reels")
+                            .orderBy("ts", Query.Direction.DESCENDING)
+                            .limit(1)
+                            .startAfter(document);
+                }
+                else if(bool.matches("2")) {
+                    query = FirebaseFirestore.getInstance()
+                            .collection("Reels")
+                            .whereEqualTo("uid", uid)
+                            .orderBy("ts", Query.Direction.DESCENDING)
+                            .limit(1)
+                            .startAfter(document);
+                }
+
+                Objects.requireNonNull(query).get().addOnCompleteListener(task1 -> {
+                    if(task1.isSuccessful() && Objects.requireNonNull(task1.getResult()).getDocuments().size() != 0) {
+                        DocumentSnapshot document1 = Objects.requireNonNull(task1.getResult()).getDocuments().get(0);
+                        ReelsPostModel reelsPostModel1 = document1.toObject(ReelsPostModel.class);
+                        Objects.requireNonNull(reelsPostModel1).setDocID(document1.getId());
+                        models.add(models.size(), reelsPostModel1);
+                    }
+                });
+
                 adapter = new ReelsAdapter(ReelsActivity.this, models, reelsList, bool, uid);
                 reelsList.setAdapter(adapter);
-            }
-        });
-    }
-
-    private void fetchAfter(String bool, DocumentSnapshot reelslastVisible) {
-        Query query = null;
-
-        if(bool.matches("1")) {
-            query = FirebaseFirestore.getInstance()
-                    .collection("Reels")
-                    .orderBy("ts", Query.Direction.DESCENDING)
-                    .limit(1)
-                    .startAfter(reelslastVisible);
-        }
-        else if(bool.matches("2")) {
-            query = FirebaseFirestore.getInstance()
-                    .collection("Reels")
-                    .whereEqualTo("uid", uid)
-                    .orderBy("ts", Query.Direction.DESCENDING)
-                    .limit(1)
-                    .startAfter(reelslastVisible);
-        }
-
-        Objects.requireNonNull(query).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful() && Objects.requireNonNull(task.getResult()).getDocuments().size() != 0) {
-                ArrayList<ReelsPostModel> reelsPostModels = new ArrayList<>();
-                DocumentSnapshot document = Objects.requireNonNull(task.getResult()).getDocuments().get(0);
-                ReelsPostModel reelsPostModel = document.toObject(ReelsPostModel.class);
-                Objects.requireNonNull(reelsPostModel).setDocID(document.getId());
-                reelsPostModels.add(reelsPostModel);
-                models.addAll(models.size(), reelsPostModels);
-            }
-        });
-    }
-
-    private void fetchBefore(String bool, DocumentSnapshot reelslastVisible) {
-        Query query = null;
-
-        if(bool.matches("1")) {
-            query = FirebaseFirestore.getInstance()
-                    .collection("Reels")
-                    .orderBy("ts", Query.Direction.ASCENDING)
-                    .limit(1)
-                    .startAfter(reelslastVisible);
-        }
-        else if(bool.matches("2")) {
-            query = FirebaseFirestore.getInstance()
-                    .collection("Reels")
-                    .whereEqualTo("uid", uid)
-                    .orderBy("ts", Query.Direction.ASCENDING)
-                    .limit(1)
-                    .startAfter(reelslastVisible);
-        }
-
-        Objects.requireNonNull(query).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful() && Objects.requireNonNull(task.getResult()).getDocuments().size() != 0) {
-                ArrayList<ReelsPostModel> reelsPostModels = new ArrayList<>();
-                DocumentSnapshot document = Objects.requireNonNull(task.getResult()).getDocuments().get(0);
-                ReelsPostModel reelsPostModel = document.toObject(ReelsPostModel.class);
-                Objects.requireNonNull(reelsPostModel).setDocID(document.getId());
-                reelsPostModels.add(reelsPostModel);
-                models.addAll(0, reelsPostModels);
             }
         });
     }
