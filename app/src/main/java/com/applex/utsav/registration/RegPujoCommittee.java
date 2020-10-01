ed3812ce1bcf82cc4b6ef9e688c02ef14056d3bb
Sourajit <amisourajit123@gmail.com>
2020-10-02 03:48:59 +0530

    init

--- app/src/main/java/com/applex/utsav/registration/RegPujoCommittee.java
+++ app/src/main/java/com/applex/utsav/registration/RegPujoCommittee.java
@@ -499,7 +499,7 @@ public class RegPujoCommittee extends AppCompatActivity {
                 }
 
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                compressedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                compressedBitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos);
 
 //                Bitmap bitmap = null;
 //                try {
