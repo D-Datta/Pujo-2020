28a52b8221361e5bd8948890f6829498437eac71
prajata22 <prajatasamanta22@gmail.com>
2020-10-11 14:19:44 +0530

    Pujo App

--- app/src/main/java/com/applex/utsav/utility/BasicUtility.java
+++ app/src/main/java/com/applex/utsav/utility/BasicUtility.java
@@ -174,7 +174,7 @@ public class BasicUtility {
     }
 
     //612, 816
-    public static Bitmap decodeSampledBitmapFromFile(Bitmap bitmap, int reqWidth, int reqHeight) {
+    public static Bitmap decodeSampledBitmapFromFile(Bitmap bitmap, int reqWidth, int reqHeight) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         Objects.requireNonNull(bitmap).compress(Bitmap.CompressFormat.JPEG, 60, out);
         byte[] bytes = out.toByteArray();
