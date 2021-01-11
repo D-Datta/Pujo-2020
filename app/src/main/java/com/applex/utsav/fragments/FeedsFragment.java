345b965b4cccd264d2faeeb845ca5a6e1c4e8a34
Sourajit1999 <amisourajit123@gmail.com>
2021-01-12 02:40:34 +0530

    init

--- app/src/main/java/com/applex/utsav/fragments/FeedsFragment.java
+++ app/src/main/java/com/applex/utsav/fragments/FeedsFragment.java
@@ -185,6 +185,8 @@ public class FeedsFragment extends Fragment {
             buildRecyclerView();
         });
         //SWIPE REFRESH//
+
+        buildRecyclerView();
     }
 
     private void buildRecyclerView() {
