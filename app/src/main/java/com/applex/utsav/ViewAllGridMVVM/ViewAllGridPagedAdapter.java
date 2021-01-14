ff82087d75127835e04fd7abbb679ea02f862ad0
prajata22 <prajatasamanta22@gmail.com>
2021-01-14 19:41:24 +0530

    Commit

--- app/src/main/java/com/applex/utsav/ViewAllGridMVVM/ViewAllGridPagedAdapter.java
+++ app/src/main/java/com/applex/utsav/ViewAllGridMVVM/ViewAllGridPagedAdapter.java
@@ -554,22 +554,13 @@ public class ViewAllGridPagedAdapter extends PagedListAdapter<DocumentSnapshot, 
                                 player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
                                 player.prepare();
                                 AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                                if(audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                                if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
                                     player.start();
-                                    if(!player.isPlaying()) {
-                                        programmingViewHolder.dhak_anim.cancelAnimation();
-                                        programmingViewHolder.dhak_anim.setVisibility(View.GONE);
-                                    }
-                                    player.setOnCompletionListener(mediaPlayer -> {
-                                        programmingViewHolder.dhak_anim.cancelAnimation();
-                                        programmingViewHolder.dhak_anim.setVisibility(View.GONE);
-                                    });
-                                } else {
-                                    new Handler().postDelayed(() -> {
-                                        programmingViewHolder.dhak_anim.cancelAnimation();
-                                        programmingViewHolder.dhak_anim.setVisibility(View.GONE);
-                                    }, 2000);
                                 }
+                                new Handler().postDelayed(() -> {
+                                    programmingViewHolder.dhak_anim.cancelAnimation();
+                                    programmingViewHolder.dhak_anim.setVisibility(View.GONE);
+                                }, player.getDuration());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
